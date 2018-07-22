0eabbae0f1bdfc473fc7cb43fdda36243c81ddf4
Nokan Page <nokanpage@gmail.com>
2018-07-22 14:36:35 -0400

    add prism syntax highlighting plugin

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -4,6 +4,8 @@ import Helmet from 'react-helmet';
 
 import Header from '../components/Header';
 import './index.css';
+import './prism.css';
+
 
 const TemplateWrapper = ({ children }) => (
   <div>

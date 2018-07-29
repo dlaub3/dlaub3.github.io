1b9b7608d99740eea553225243f04da6f5582dbb
Dan <dlaubacher3@gmail.com>
2018-07-29 15:22:40 -0400

    Add github gist support

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,11 +13,19 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-embed-gist',
+            options: {
+              username: 'dlaub3',
+              // default: true
+              includeDefaultCss: true,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
             options: {
               // Class prefix for <pre> tags containing syntax highlighting;
               // defaults to 'language-' (eg <pre class="language-js">).
@@ -26,7 +34,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: "",
+              classPrefix: '',
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
